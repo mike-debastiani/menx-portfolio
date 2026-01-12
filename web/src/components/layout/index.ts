4566688388eb639319188fd9210806dccee749be
mike-debastiani <mike.debastiani@stud.hslu.ch>
2026-01-12 22:33:16 +0100

    fix: correct Header export in layout index
    - Change from default export to named export to match Header/index.ts structure

--- web/src/components/layout/index.ts
+++ web/src/components/layout/index.ts
@@ -1,4 +1,4 @@
 export { default as Container } from './Container';
 export { default as Grid } from './Grid';
 export { default as Col } from './Col';
-export { default as Header } from './Header';
+export { Header } from './Header';
